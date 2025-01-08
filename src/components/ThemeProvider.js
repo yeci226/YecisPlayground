64418a8faa7a3e975665dc821a1efc1b60e8b466
yeci226 <shawnyin226@gmail.com>
2025-01-08 11:21:24 +0800

    [Global] FIx theme unsaved

--- src/components/ThemeProvider.js
+++ src/components/ThemeProvider.js
@@ -6,23 +6,33 @@ export function ThemeProvider({ children }) {
   const [theme, setTheme] = useState("light");
 
   useEffect(() => {
-    const savedTheme = localStorage.getItem("theme") || "light";
-    setTheme(savedTheme);
+    if (typeof window !== "undefined") {
+      const savedTheme = localStorage.getItem("theme") || "light";
+      setTheme(savedTheme);
+
+      if (savedTheme === "dark") document.documentElement.classList.add("dark");
+    }
   }, []);
 
+  useEffect(() => {
+    theme === "dark"
+      ? document.documentElement.classList.add("dark")
+      : document.documentElement.classList.remove("dark");
+  }, [theme]);
+
   const toggleTheme = async () => {
-    if (document.startViewTransition) {
-      await document.startViewTransition(async () => {
-        const newTheme = theme === "light" ? "dark" : "light";
+    const newTheme = theme === "light" ? "dark" : "light";
+
+    if (typeof window !== "undefined") {
+      if (document.startViewTransition) {
+        await document.startViewTransition(async () => {
+          setTheme(newTheme);
+          localStorage.setItem("theme", newTheme);
+        }).finished;
+      } else {
         setTheme(newTheme);
-        document.documentElement.classList.toggle("dark");
         localStorage.setItem("theme", newTheme);
-      }).finished;
-    } else {
-      const newTheme = theme === "light" ? "dark" : "light";
-      setTheme(newTheme);
-      document.documentElement.classList.toggle("dark");
-      localStorage.setItem("theme", newTheme);
+      }
     }
   };
 
